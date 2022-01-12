6855788bfeeac3ecf4fcadfa55b63d425a307d45
Gabriel Lacroix <lacroixgabriel@gmail.com>
2022-01-12 13:17:55 -0500

    Create view for item details

--- inventorytracker/inventory/views.py
+++ inventorytracker/inventory/views.py
@@ -8,3 +8,9 @@ def index(request):
     all_items = InventoryItem.objects.all().order_by("name")
     return render(request, "inventory/items.html", {"items": all_items})
 
+
+def item_detail(request, item_id):
+    item = get_object_or_404(InventoryItem, pk=item_id)
+    return render(request, "inventory/item_detail.html", {"item": item})
+
+
