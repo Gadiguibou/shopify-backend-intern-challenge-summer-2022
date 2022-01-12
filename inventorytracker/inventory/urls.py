c080817ab32ec5cba830ee9d34df04a51a6f1c54
Gabriel Lacroix <lacroixgabriel@gmail.com>
2022-01-12 13:23:48 -0500

    Create view to edit item details

--- inventorytracker/inventory/urls.py
+++ inventorytracker/inventory/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("item/<int:item_id>/", views.item_detail, name="item_detail"),
+    path("item/<int:item_id>/edit/", views.item_edit, name="item_edit"),
+    path(
+        "item/<int:item_id>/edit/submit/", views.item_edit_form, name="item_edit_form"
+    ),
 ]
--- inventorytracker/inventory/views.py
+++ inventorytracker/inventory/views.py
@@ -14,3 +14,50 @@ def item_detail(request, item_id):
     return render(request, "inventory/item_detail.html", {"item": item})
 
 
+def item_edit(request, item_id):
+    item = get_object_or_404(InventoryItem, pk=item_id)
+    return render(request, "inventory/item_edit.html", {"item": item})
+
+
+def item_edit_form(request, item_id):
+    item = get_object_or_404(InventoryItem, pk=item_id)
+
+    try:
+        name = request.POST["name"]
+        description = request.POST["description"]
+        quantity = request.POST["quantity"]
+        error_message = _validate_item_form(name, quantity, description)
+        if error_message is not None:
+            return render(
+                request,
+                "inventory/item_create.html",
+                {"error": error_message},
+            )
+        else:
+            item.name = name
+            item.description = description
+            item.quantity = quantity
+    except KeyError:
+        return render(
+            request,
+            "inventory/item_edit.html",
+            {"item": item, "error": "Please fill in all the required fields."},
+        )
+
+    item.save()
+    return HttpResponseRedirect(reverse("item_detail", args=(item.id,)))
+
+
+
+
+def _validate_item_form(name, quantity, description):
+    error_message = ""
+
+    if name is None or quantity is None or description is None:
+        error_message += "Please fill in all the required fields."
+    if len(name) == 0 or len(name) > 100:
+        error_message += "Please fill in the name field."
+    if int(quantity) < 0:
+        error_message += "Please enter a positive number for the quantity."
+
+    return error_message if len(error_message) > 0 else None
